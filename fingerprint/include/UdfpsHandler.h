9d2c2b6d5c8ddc81b63989f59b2e21beccb72248
AdarshGrewal <adarshgrewal@gmail.com>
2025-09-22 13:09:33 +0530

    fingerprint: Use correct header for UdfpsHandler
    Change-Id: I8e8512550e9d62d2797774aaa556ea27e71013cd

--- fingerprint/include/UdfpsHandler.h
+++ fingerprint/include/UdfpsHandler.h
@@ -7,7 +7,12 @@
 #pragma once
 
 #include <stdint.h>
+
+#ifndef DEVICE_USES_NEW_IMPLEMENTATION
+#include "fingerprint-legacy.h"
+#else
 #include "fingerprint.h"
+#endif
 
 class UdfpsHandler {
   public:
